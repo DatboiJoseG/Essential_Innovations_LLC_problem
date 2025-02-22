d64de4b746f5d80a08021b0db086b4f5ad513c8d
Dalton Powell <wdpowell03@gmail.com>
2025-02-22 09:39:57 -0500

    Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from "./LandingPage"
+import SearchBar from "./SearchBar"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <LandingPage />
+      <SearchBar />
     </>
   )
 }
---
+++ src/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
+        onSearch(searchTerm);
+    };
+
+    return (
+        <form onSubmit={handleSearchSubmit}>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Search..."
+            />
+            <button type="submit">Search</button>
+        </form>
+    );
+}
